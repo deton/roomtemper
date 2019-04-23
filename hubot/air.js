8fc6a04b6f39d342f6e8eaae1849b2c61df86081
KIHARA Hideto <deton@m1.interq.or.jp>
2019-04-23 19:34:28 +0900

    hubotスクリプト更新:CO2濃度も含める

--- hubot/air.js
+++ hubot/air.js
@@ -1,33 +1,39 @@
 // Description
-//   A hubot script that returns room temperature and window open/close status
+//   A hubot script that returns room temperature, CO2 and window open/close status
 //
 // Commands:
-//   air - return room temperature and window open/close status
+//   air - return room temperature, CO2 and window open/close status
 //
 // Author:
 //   KIHARA Hideto <deton@m1.interq.or.jp>
 
 module.exports = function (robot) {
   robot.hear(/^air/i, function (msg) {
-    var q = encodeURIComponent("SELECT LAST(value) FROM temperature WHERE sensor='STM431J_01'; SELECT LAST(value) FROM window WHERE sensor='win7'");
+    var q = encodeURIComponent("SELECT LAST(value) FROM temperature WHERE sensor='STM431J_01'; SELECT LAST(value) FROM window WHERE sensor='win7'; SELECT LAST(value) FROM co2 WHERE sensor='MHZ14A_01'");
     var req = robot.http('http://localhost:8086/query?epoch=ms&db=roomdb&q=' + q).get();
     req(function (err, res, body) {
       if (err) {
         robot.logger.warning('Failed to get room temperature:' + error);
         return;
       }
+      //console.log(body);
       var json = JSON.parse(body);
       // {"results":[
       //   {"series":[{"name":"temperature","columns":["time","last"],"values":[[1515210895000,10.7]]}]},
-      //   {"series":[{"name":"window","columns":["time","last"],"values":[[1515399747622,0]]}]}]}
+      //   {"series":[{"name":"window","columns":["time","last"],"values":[[1515399747622,0]]}]},
+      //   {"series":[{"name":"co2","columns":["time","last"],"values":[[1515399747622,929]]}]}]}
       var tempdata = json.results[0].series[0].values[0];
       var windata = json.results[1].series[0].values[0];
+      var co2data = json.results[2].series[0].values[0];
    
       if (isOld(tempdata[0])) {
         robot.logger.warning('too old data:' + time);
         return;
       }
       var m = 'Temperature: ' + tempdata[1] + '℃ [STM431J at S307]';
+      if (!isOld(co2data[0])) {
+        m += ', CO2: ' + co2data[1] + 'ppm';
+      }
       if (!isOld(windata[0])) {
         if (windata[1] == 1) {
           m += ' (window is OPEN)';
